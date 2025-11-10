fed8cc666b9f11bf3f389f654109b19e34dc0cea
Preacher-Y <ysheja@gmail.com>
2025-11-11 01:45:24 +0200

    feat: Added the Header Component which is responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "FoodWagen",
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
